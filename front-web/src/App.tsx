050286ff2e4103d36ff24d1a7394ab943c41336d
RodrigoxAlmeida <rodrigox.almeida@yahoo.com.br>
2021-01-09 20:21:31 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
 import Navbar from './Navbar';
-
+import Home from './Home';
 
 function App() {
   return (
-    <div>
-        
-    <Navbar />
-    
-    
+    <>
+    <div>        
+      <Navbar />
+      <Home/>    
     </div>
-     );
+      </>
+   );
 }
 
 export default App;
